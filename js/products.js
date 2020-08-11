0a1602be0a24958a681c82f552e3c12b597a0153
Tomás <deleontomas99@gmail.com>
2020-08-10 22:06:00 -0300

    change

--- js/products.js
+++ js/products.js
@@ -1,3 +1,59 @@
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action" onclick="desplegar(LIST_URL2)">
+            <div class="row" id="` + category.name + `">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + category.productCount + ` artículos</small>
+                    </div>
+                    <div> ` + category.description + `</div>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+function showCategoriesList2(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row" id="` + category.name + `">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted"> costo: usd$` + category.cost + `</small>
+                    </div>
+                    <div> ` + category.description + `</div>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
